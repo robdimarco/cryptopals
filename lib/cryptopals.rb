60a3fbcd0d8bdcbff560e69985236e8b9237f9b2
Rob Di Marco <rob@innovationontherun.com>
2020-07-04 12:44:29 -0400

    Rubocop auto correct

--- lib/cryptopals.rb
+++ lib/cryptopals.rb
@@ -1,5 +1,7 @@
-def str_to_hex(s)
-  s.scan(/../).map {|x| x.hex}
+# frozen_string_literal: true
+
+def hex_to_binary(s)
+  s.scan(/../).map(&:hex)
 end
 
 # https://cryptopals.com/sets/1
@@ -9,7 +11,7 @@ def str_to_bin(s)
 end
 
 def hex_to_str(bytes)
-  bytes.map {|i| '%02x' % i}.join
+  bytes.map { |i| '%02x' % i }.join
 end
 
 def xor_str(s1, s2)
@@ -24,7 +26,7 @@ def xor_bytes(b1, b2)
 end
 
 def sentence_score(s)
-  str_to_bin(s).chars.map{|i| i.match(/[\w\s\d]/) ? 1 : 0}.sum / s.length.to_f
+  str_to_bin(s).chars.map { |i| i.match(/[\w\s\d]/) ? 1 : 0 }.sum / s.length.to_f
 end
 
 def potential_strings_from_single_char_xor(s)
@@ -41,25 +43,18 @@ def encrypt(s, key)
 end
 
 def hamming_distance(s1, s2)
-  s1.bytes.zip(s2.bytes).map {|(a,b)| (a^b).to_s(2)}.join.scan('1').size
-end
-
-
-def prob1
-  s="49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-  require 'base64'
-  Base64.strict_encode64(str_to_bin(s))
+  s1.bytes.zip(s2.bytes).map { |(a, b)| (a ^ b).to_s(2) }.join.scan('1').size
 end
 
 def prob2
-  s= "1c0111001f010100061a024b53535009181c"
-  xor = "686974207468652062756c6c277320657965"
+  s = '1c0111001f010100061a024b53535009181c'
+  xor = '686974207468652062756c6c277320657965'
   xor_str(s, xor)
 end
 
 def prob3
-  s = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-  vals = potential_strings_from_single_char_xor(s).sort_by {|(_, s)| -1 * sentence_score(s)}
+  s = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+  vals = potential_strings_from_single_char_xor(s).sort_by { |(_, s)| -1 * sentence_score(s) }
 
   vals[0]
 end
@@ -70,7 +65,7 @@ def prob4
   pairs = strings.flat_map do |s|
     potential_strings_from_single_char_xor(s)
   end
-  best = pairs.sort_by {|(_, s)| -1 * sentence_score(s)}[0]
+  best = pairs.min_by { |(_, s)| -1 * sentence_score(s) }
   [str_to_bin(best[1]), best[0], best[1]]
 end
 
@@ -82,6 +77,5 @@ end
 def prob6
   data = ''
   (2..40).each do |keysize|
-
   end
 end
